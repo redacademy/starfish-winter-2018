b794588a666c482188f74feece8bd169c8e373db
Paul Dudsdeemaytha <pauldudsdeemaytha@Pauls-MacBook-Pro.local>
2018-03-15 14:15:24 -0700

    added flexboxes to header, styled header and adjusted margins

--- themes/thestarfish/js/open-side-menu.js
+++ themes/thestarfish/js/open-side-menu.js
@@ -1,11 +1,26 @@
 (function($) {
 
+// $( "#menu-open" ).click(function() {
+//     document.getElementById("site-navigation").style.width = "100%";
+// });
+
+// $( "#menu-close" ).click(function() {
+//     document.getElementById("site-navigation").style.width = "0";
+// })
+
 $( "#menu-open" ).click(function() {
-    document.getElementById("site-navigation").style.width = "100%";
-});
+    // $("#site-navigation").addClass("open-mobile");
+    // if($("#site-navigation").hasClass("open-mobile")) {
+    $("#site-navigation").toggle("fast");
+    event.preventDefault();
+    }
+);
 
 $( "#menu-close" ).click(function() {
-    document.getElementById("site-navigation").style.width = "0";
-})
+    // if($("#site-navigation").hasClass("open-mobile")) {
+    $("#site-navigation").toggle("fast");
+    event.preventDefault();
+    }
+);
 
 })( jQuery );
