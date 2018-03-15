b7cd5b00f19a67a704c97e984bf6cc1e346d7672
Paul Dudsdeemaytha <pauldudsdeemaytha@Pauls-MacBook-Pro.local>
2018-03-15 10:37:39 -0700

    added temp styling to nav menu mobile

--- themes/thestarfish/header.php
+++ themes/thestarfish/header.php
@@ -22,7 +22,9 @@
 
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-logo">
-					<img src="<?php echo get_template_directory_uri() . '/images/logo.png'; ?>" class="logo" alt="Starfish Logo">
+					<a href="<?php echo get_home_url(home_url( '/' )); ?>">
+						<img src="<?php echo get_template_directory_uri() . '/images/logo.png'; ?>" class="logo" alt="Starfish Logo">
+					</a>
 				</div>
 				<div class="site-branding">
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>					
@@ -33,10 +35,13 @@
 				</a>
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
-					<a href="#" class="btn-close" id="menu-close">&times;</a>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) );  ?>
-					<p>Donate</p>
+					<div class="nav-content">
+						<a href="#" class="btn-close" id="menu-close">&times;</a>
+						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) );  ?>
+						<div class="nav-donate">
+							<p>Donate</p>
+						</div>
+					</div>
 				</nav><!-- #site-navigation -->
 			</header><!-- #masthead -->
 
