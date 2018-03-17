c8fb1e0cfc77fe9674d58dc643e871d67caede2f
Veronika Kochetkova <veronica90k@gmail.com>
2018-03-17 14:44:26 -0700

    Set most of the php rules, including the carousel loops, in the about.php

--- themes/thestarfish/about.php
+++ themes/thestarfish/about.php
@@ -8,65 +8,57 @@
 get_header(); ?>
 
 <div id="primary" class="content-area">
-
 	<main id="main" class="site-main" role="main">
-
-<?php
-
-	/**
-	* Set the Custom Field Suite Loops Working
-	*/
-
-	$fields = CFS()->get( 'about_carousel' );
-	
-	foreach ( $fields as $field ) {
-		// d($field);
-		echo '<h2>' . $field['about_carousel_title'] . '</h2>';
-		$loop2 = $field['about_carousel_cell'];
-		// d($loop2);
-
-		echo '<div class="carousel-container">';
-		foreach($loop2 as $loop){
-			// d($loop['about_carousel_cell_image']);
-			// wrap in carousel cell
-			echo '<img src="' . $loop['about_carousel_cell_image'] . '"/>';
-			echo '<p>' . $loop['about_carousel_cell_content'] . '</p>';
-		}
-		echo '</div>';
-
-		// $cells = CFS()->get($field['about_carousel_cell']);
-		// d($cells);
-		// foreach ( $cells as $cell ) {
-		// 	echo $cell['about_carousel_cell_image'];
-		// 	echo $cell['about_carousel_cell_content'];
-		// }
-	}
-?>
-
-
-
-	<?php if ( have_posts() ) : ?>
-		<?php if ( is_home() && ! is_front_page() ) : ?>
-			<header class='entry-header'>
-				<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>					
-			</header>	
-		<?php endif; ?>
 
 		<?php /* Start the Loop */ ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'template-parts/content' ); ?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <header class="entry-header" >
+                    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+					
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+					
+                </header><!-- .entry-header -->
 
-		<?php endwhile; ?>
+                <div class="entry-content">
+					<?php the_content(); ?>
+					<?php
+						/**
+						* Set the Custom Field Suite Loops Working
+						*/
 
-		<?php the_posts_navigation(); ?>
+						$carousels = CFS()->get( 'about_carousel' );
 
-	<?php else : ?>
+						foreach ( $carousels as $carousel ) {
+							echo '<div class="carousel-container">';
+								//wrap in whole carousel
+								echo '<h2>' . $carousel['about_carousel_title'] . '</h2>';
+								$carousel_cells = $carousel['about_carousel_cell'];
+								foreach($carousel_cells as $carousel_cell){
+									echo '<div class="carousel-cell-container">';
+									// wrap in carousel cell
+										echo '<img src="' . $carousel_cell['about_carousel_cell_image_primary'] . '" class="carousel-cell-image-primary"/>';
+										echo '<img src="' . $carousel_cell['about_carousel_cell_image_secondary'] . '" class="carousel-cell-image-secondary"/>';
+										echo '<div class="carousel-cell-content">';
+											echo $carousel_cell['about_carousel_cell_content'];
+										echo '</div>';
+									echo '</div>';
+								}
+							echo '</div>';
+						}
+					?>
+					<?php
+						wp_link_pages( array(
+							'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+							'after'  => '</div>',
+						) );
+					?>
+                </div><!-- .entry-content -->
+            </article><!-- #post-## -->
 
-		<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-	<?php endif; ?>
+		<?php endwhile; // End of the loop. ?>
 
 	</main><!-- #main -->
 </div><!-- #primary -->
