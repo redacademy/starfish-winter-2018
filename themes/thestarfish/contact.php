1c6574eda92a440cba18a5e2aa4c868d2b62a65e
Audrobot <kornickiaa@gmail.com>
2018-03-19 14:38:14 -0700

    add the content

--- themes/thestarfish/contact.php
+++ themes/thestarfish/contact.php
@@ -9,9 +9,10 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 
 		<main id="main" class="contact" role="main">
-		
+
+			<?php the_content(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 <?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php get_footer(); ?>s
