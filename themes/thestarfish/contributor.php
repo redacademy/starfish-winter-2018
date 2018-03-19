2582612c6ab2aa09af24ad69f4b53532c0010a77
Audrobot <kornickiaa@gmail.com>
2018-03-19 16:37:45 -0700

    add CFS get form title and form

--- themes/thestarfish/contributor.php
+++ themes/thestarfish/contributor.php
@@ -7,43 +7,21 @@
 
 
 get_header(); ?>
-	<div id="primary" class="content-area">
 
+	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
-
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header class='entry-header'>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-					
-				</header>
-				
-			<?php endif; ?>
-
-			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'template-parts/content' ); ?>
-				
-				<?php echo $image = CFS()->get('volunteer_roles_image');?>
- 				<?php echo '<img src="' . $image . '" />'; ?>
-				<?php echo CFS()->get( 'volunteer_roles_content' ); ?>
-				<?php echo CFS()->get( 'volunteer_perks_content' ); ?>
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
-				
+				<?php echo CFS()->get( 'form_title' ); ?>
+				<?php echo CFS()->get( 'apply_here' ); ?>
 
-			<?php endwhile; ?>
-
-			<?php the_posts_navigation(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-		<?php endif; ?>
+			<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-<?php get_template_part( 'template-parts/content-donation', 'donation' ); ?>
+
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
