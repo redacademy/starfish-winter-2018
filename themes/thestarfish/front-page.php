de4fcb50279767bde01a4982004395e5e54a24b7
Paul Dudsdeemaytha <pauldudsdeemaytha@Pauls-MacBook-Pro.local>
2018-03-19 14:58:05 -0700

    background img absolute

--- themes/thestarfish/front-page.php
+++ themes/thestarfish/front-page.php
@@ -33,9 +33,44 @@ get_header(); ?>
 
 		<?php endif; ?>
 
+		<?php
+						/**
+						* Set the Custom Field Suite Loops Working
+						*/
+
+						$carousels = CFS()->get( 'front_page_carousel' );
+
+						foreach ( $carousels as $carousel ) {
+							
+							$single_img = empty($carousel['front_page_carousel_cell'][0]['front_page_carousel_cell_image_secondary']); //check if the first cell in the carousel contains one or two imgs
+
+							echo '<div class="carousel-container ' . ($single_img ? 'container-single' : 'container-double') . '">'; //if there is no secondary img -> add class container-single, otherwise -> add class container-double
+								//wrap in whole carousel
+								$carousel_cells = $carousel['front_page_carousel_cell'];
+								foreach($carousel_cells as $carousel_cell){
+									// loop for each carousel cell
+									
+									echo '<div class="carousel-cell-container' . ($single_img ? ' single' : ' double') . '">'; //if there is no secondary img -> add class single, otherwise -> double
+									//wrap in carousel cell with all the content and img
+										echo '<div class="carousel-cell-images' . ($single_img ? '' : ' carousel-double') . '">'; //if there is no secondary img -> add nothing, otherwise -> add class carousel-double
+											echo '<img src="' . $carousel_cell['front_page_carousel_cell_image_primary'] . '" class="carousel-cell-image-primary"/>';
+											if ($single_img == false) { //if there is secondary img -> show the secondary img on screen
+												echo '<img src="' . $carousel_cell['front_page_carousel_cell_image_secondary'] . '" class="carousel-cell-image-secondary"/>';
+											}
+										echo '</div>';
+										echo '<div class="carousel-cell-content">';
+											echo '<h2>' . $carousel['front_page_carousel_title'] . '</h2>';
+											echo $carousel_cell['front_page_carousel_cell_content'];
+										echo '</div>';
+									echo '</div>';
+								}
+							echo '</div>';
+						}
+					?>
+
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
 <?php get_sidebar(); ?>
 <?php get_template_part( 'template-parts/content', 'donation' ); ?>
 <?php get_footer(); ?>
