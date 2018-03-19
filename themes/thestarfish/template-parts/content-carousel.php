9d489f833122a18b409b84b9bbe80f9c24b55e21
Veronika Kochetkova <veronica90k@gmail.com>
2018-03-19 11:58:21 -0700

    Copy the carousel code and paste it in the content-carousel.php template part

--- themes/thestarfish/template-parts/content-carousel.php
+++ themes/thestarfish/template-parts/content-carousel.php
@@ -7,30 +7,42 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php if ( has_post_thumbnail() ) : ?>
-			<?php the_post_thumbnail( 'large' ); ?>
-		<?php endif; ?>
+<?php
 
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	/**
+	 * Template Part for the carousel custom fields. COPY, PASTE, AND CHANGE for the page you are working on.
+	 */
 
-		<div class="entry-meta">
-			<?php starfish_posted_on(); ?> / <?php starfish_comment_count(); ?> / <?php starfish_posted_by(); ?>
-		</div><!-- .entry-meta -->
-	</header><!-- .entry-header -->
+	/**
+	* Set the Custom Field Suite Loops Working
+	*/
 
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+	$carousels = CFS()->get( 'about_carousel' );
 
-	<footer class="entry-footer">
-		<?php starfish_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-## -->
+	foreach ( $carousels as $carousel ) {
+		
+		$single_img = empty($carousel['about_carousel_cell'][0]['about_carousel_cell_image_secondary']); //check if the first cell in the carousel contains one or two imgs
+
+		echo '<div class="carousel-container ' . ($single_img ? 'container-single' : 'container-double') . '">'; //if there is no secondary img -> add class container-single, otherwise -> add class container-double
+			//wrap in whole carousel
+			$carousel_cells = $carousel['about_carousel_cell'];
+			foreach($carousel_cells as $carousel_cell){
+				// loop for each carousel cell
+				
+				echo '<div class="carousel-cell-container' . ($single_img ? ' single' : ' double') . '">'; //if there is no secondary img -> add class single, otherwise -> double
+				//wrap in carousel cell with all the content and img
+					echo '<div class="carousel-cell-images' . ($single_img ? '' : ' carousel-double') . '">'; //if there is no secondary img -> add nothing, otherwise -> add class carousel-double
+						echo '<img src="' . $carousel_cell['about_carousel_cell_image_primary'] . '" class="carousel-cell-image-primary"/>';
+						if ($single_img == false) { //if there is secondary img -> show the secondary img on screen
+							echo '<img src="' . $carousel_cell['about_carousel_cell_image_secondary'] . '" class="carousel-cell-image-secondary"/>';
+						}
+					echo '</div>';
+					echo '<div class="carousel-cell-content">';
+						echo '<h2>' . $carousel['about_carousel_title'] . '</h2>';
+						echo $carousel_cell['about_carousel_cell_content'];
+					echo '</div>';
+				echo '</div>';
+			}
+		echo '</div>';
+	}
+?>
