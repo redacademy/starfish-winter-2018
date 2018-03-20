97b9d7e04e125d22bdd84a5a1d7d94c55fc540ba
Audrobot <kornickiaa@gmail.com>
2018-03-20 14:03:00 -0700

    add donate page

--- themes/thestarfish/donate.php
+++ themes/thestarfish/donate.php
@@ -4,3 +4,17 @@
  *
  * @package starfish_Theme
  */
+
+?>
+	<div id="primary" class="content-area"
+		<main id="main" class="content-main" role="main">
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'template-parts/content' ); ?>
+
+			<?php endwhile; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
