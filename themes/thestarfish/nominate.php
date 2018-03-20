60c3cb03b6b6a8314c9f8f7ba00a7369da1786b6
Audrobot <kornickiaa@gmail.com>
2018-03-20 13:59:10 -0700

    add nominate page

---
+++ themes/thestarfish/nominate.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template Name: Nominate Page
+ *
+ * @package starfish_Theme
+ */
+
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+			<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
