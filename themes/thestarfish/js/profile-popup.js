a0c4385754b8186c9652391b5131f6db6c39c504
Paul Dudsdeemaytha <pauldudsdeemaytha@Pauls-MacBook-Pro.local>
2018-03-21 11:29:27 -0700

    added profile lightbox functionality

---
+++ themes/thestarfish/js/profile-popup.js
@@ -0,0 +1,48 @@
+(function($){
+
+    /**
+     * Get post id and create a modal/lightbox for team members
+     */
+    $('.profile-picture').on('click', function(event){
+        event.preventDefault();
+
+       var postId = $(this).attr('id');
+       console.log(postId);
+    //    alert(postId);
+
+    $.ajax({
+        method: "GET",
+        url: "http://localhost:8888/starfish/wp-json/wp/v2/profile/" + postId,
+    }).done(function(data){
+        console.log(data);
+
+        var teamTitle = data.title.rendered;
+
+        var htmlTemplate = '<div class="sf-modal">';
+        htmlTemplate += '<div id="sf-modal-close"><i class="fas fa-times"></i></div>';
+        htmlTemplate += '<h2>' + teamTitle + '</h2>';
+        htmlTemplate += '</div>';
+
+        $('body').append(htmlTemplate);
+
+        $('#sf-modal-close').on('click', function(){
+            var animTiming = 2000;
+            $('.sf-modal').fadeOut(animTiming);
+            setTimeout(function(){
+                $('.sf-modal').remove();
+            }, animTiming);
+
+
+            
+        });
+
+    })
+    .fail(function(err){
+        console.log(err);
+    });
+
+
+       
+    });// / .profile-picture').on('click'
+
+})(jQuery);
