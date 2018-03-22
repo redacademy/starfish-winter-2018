234ecc09ad71c0d6bedb18275d0b6436952d042b
Paul Dudsdeemaytha <pauldudsdeemaytha@Pauls-MacBook-Pro.local>
2018-03-22 10:34:42 -0700

    removed white space top of page

--- themes/thestarfish/js/open-side-menu.js
+++ themes/thestarfish/js/open-side-menu.js
@@ -12,10 +12,34 @@ $( "#menu-open" ).click(function() {
 );
 
 //Changes the + to - when clicked
-$( "#open-menu-link fa-plus").click(function(){
-    // $(".main-navigation ul ul").toggleClass( "show-sub-menu");
-    console.log("works i guess");
+function isTouchDevice(){
+    return typeof window.ontouchstart !== 'undefined';
+}
+
+$(document).ready(function(){
+    /* If mobile browser, prevent click on parent nav item from redirecting to URL */
+    if(isTouchDevice()) {
+        // 1st click, add "clicked" class, preventing the location change. 2nd click will go through.
+        $("#menu-main-navigation > li > a").click(function(event) {
+            // Perform a reset - Remove the "clicked" class on all other menu items
+            $("#menu-main-navigation > li > a").not(this).removeClass("clicked");
+            $(this).toggleClass("clicked");
+            if ($(this).hasClass("clicked")) {
+                event.preventDefault();
+                $(".main-navigation ul ul").css({
+                    "left" : "0",
+                });
+            }
+            if ($(this).hasClass("clicked")) {
+                event.preventDefault();
+                $(".main-navigation ul").css({
+                    "display" : "block",
+                });
+            }
+        });
+    }
 });
+
 
 //On mobile, when clicking on the X, will remove each + symbol on each submenu item
 $( "#menu-close" ).click(function() {
