a35faccbaa2757b196916dc4dd156d7616849e28
Veronika Kochetkova <veronica90k@gmail.com>
2018-03-21 18:45:46 -0700

    Change the donate button styling in the donation template part and in the forms.scss

--- themes/thestarfish/template-parts/content-donation.php
+++ themes/thestarfish/template-parts/content-donation.php
@@ -13,9 +13,11 @@
 			<h1>You can Help!</h1>
 			<p>Your support will help us run our programs, provide a plateform to changemakers of tommorow and give them the recognition they deserve.</p>
 		</div>
-		<a class="btn-donate" href="<?php echo esc_url(get_permalink(get_page_by_path( 'donate' ) ) ); ?>" >
-			<p>Donate</p>
-			<span class="screen-reader-text"><?php echo esc_html( 'Donate' ); ?></span>
-		</a>
+		<div class="btn-donate">
+			<a href="<?php echo esc_url(get_permalink(get_page_by_path( 'donate' ) ) ); ?>" >
+				Donate
+				<span class="screen-reader-text"><?php echo esc_html( 'Donate' ); ?></span>
+			</a>
+		</div>
 	</div><!-- .entry-content -->
 </section><!-- end of section -->
