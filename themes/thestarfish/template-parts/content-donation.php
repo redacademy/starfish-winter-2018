20f59a731a57019e145020a8888a23443f6b4b95
Audrobot <kornickiaa@gmail.com>
2018-03-23 11:55:17 -0700

    format p

--- themes/thestarfish/template-parts/content-donation.php
+++ themes/thestarfish/template-parts/content-donation.php
@@ -11,7 +11,8 @@
 	<div class="content-donation">
 		<div class="content-donation-info">
 			<h2>You can Help!</h2>
-			<p>Your support will help us run our programs, provide a plateform to changemakers of tommorow and give them the recognition they deserve.</p>
+			<p>Your support will help us run our programs, provide a plateform to changemakers 
+				of tommorow and give them the recognition they deserve.</p>
 		</div>
 		<div class="btn-donate">
 			<a href="<?php echo esc_url(get_permalink(get_page_by_path( 'donate' ) ) ); ?>" >
