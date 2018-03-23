f79763dfe24a87eea0db754619ef4c9388c83f01
Veronika Kochetkova <veronica90k@gmail.com>
2018-03-22 22:16:43 -0700

    Style the desktop version footer

--- themes/thestarfish/footer.php
+++ themes/thestarfish/footer.php
@@ -8,48 +8,48 @@
 
 		</div><!-- #content -->
 			<footer id="colophon" class="site-footer" role="contentinfo">
-				
-				<div class="site-info">
-					
-					<div id="footer-sidebar" class="secondary">
-						<div id="footer-sidebar1">
-							
-							<?php
-								if(is_active_sidebar('footer-sidebar-1')){
-								dynamic_sidebar('footer-sidebar-1');
-								}
-							?>
+				<div class="footer-wrapper"> 
+					<div class="site-info">
 						
-						</div><!-- #footer-sidebar1 -->
-					</div><!-- #footer-sidebar -->
-					
-					<div class="footer-contact">
-						<h3>Stay in Touch</h3>
-						<div>
-							<a class="btn-transparent" href="<?php echo esc_url(get_permalink(get_page_by_path( 'contact' ) ) ); ?>">Contact Us</a>
+						<div id="footer-sidebar" class="secondary">
+							<div id="footer-sidebar1">
+								
+								<?php
+									if(is_active_sidebar('footer-sidebar-1')){
+									dynamic_sidebar('footer-sidebar-1');
+									}
+								?>
+							
+							</div><!-- #footer-sidebar1 -->
+						</div><!-- #footer-sidebar -->
+						
+						<div class="footer-contact">
+							<h3>Stay in Touch</h3>
+							<div>
+								<a class="btn-transparent" href="<?php echo esc_url(get_permalink(get_page_by_path( 'contact' ) ) ); ?>">Contact Us</a>
+							</div>
+						</div><!-- .footer-contact -->
+
+						<div class="footer-social">
+							<h3>Social Media</h3>
+							<i class="fab fa-twitter"></i>
+							<i class="fab fa-facebook"></i>
+							<i class="fab fa-instagram"></i>
+							<i class="fab fa-youtube"></i>
+						</div><!-- .footer-social -->
+
+					</div><!-- .site-info -->
+
+					<div class="site-copyright">
+						
+						<div class="site-footer-logo">
+							<img src="<?php echo get_template_directory_uri() . '/images/logo-white.svg'; ?>" class="logo" alt="Starfish Logo">
 						</div>
-					</div><!-- .footer-contact -->
-
-					<div class="footer-social">
-						<h3>Social Media</h3>
-						<i class="fab fa-twitter"></i>
-						<i class="fab fa-facebook"></i>
-						<i class="fab fa-instagram"></i>
-						<i class="fab fa-youtube"></i>
-					</div><!-- .footer-social -->
-
-				</div><!-- .site-info -->
-
-				<div class="site-copyright">
+						
+						<p>&copy; <?php echo date( 'Y' ); ?> The Starfish Environmental Society. Website by <a href="https://redacademy.com/" target="_blank">Red Academy</a></p>
 					
-					<div class="site-footer-logo">
-						<img src="<?php echo get_template_directory_uri() . '/images/logo-white.svg'; ?>" class="logo" alt="Starfish Logo">
-					</div>
-					
-					<p>&copy; <?php echo date( 'Y' ); ?> The Starfish Environmental Society. Website by <a href="https://redacademy.com/" target="_blank">Red Academy</a></p>
-				
-				</div><!-- .site-copyright -->
-
+					</div><!-- .site-copyright -->
+				</div><!-- .footer-wrapper -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
 
