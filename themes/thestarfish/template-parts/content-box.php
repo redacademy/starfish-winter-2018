974c48d7d7da0c8b36d230526d76e9e92cd3b8b0
Audrobot <kornickiaa@gmail.com>
2018-03-22 20:53:14 -0700

    wrap content box in section class

--- themes/thestarfish/template-parts/content-box.php
+++ themes/thestarfish/template-parts/content-box.php
@@ -6,17 +6,21 @@
  */
 ?>
 
-<section class="content-box">
+<section class="content-box-area">
 	
-	<div class="content-box-header">
-		<h2></h2>
-	</div><!-- .content-box-header -->
+	<div class="content-box">
+		
+		<div class="content-box-header">
+			<h2>NAME OF HEADER HERE</h2>
+		</div><!-- .content-box-header -->
 
-	<div class="content-box-content">
-		<!-- Content (description) for the box here -->
-		<a class="content-box-button" href="<?php echo esc_url(get_permalink(get_page_by_path( 'path to the page here' ) ) ); ?>"><!-- Name of the button here -->
-			<span class="screen-reader-text"><?php echo esc_html( 'NAME OF THE BUTTON HERE' ); ?></span>
-		</a><!-- .content-box-button -->
+		<div class="content-box-content">
+			<!-- Content (description) for the box here -->
+			<a class="content-box-button" href="<?php echo esc_url(get_permalink(get_page_by_path( 'path to the page here' ) ) ); ?>"><!-- Name of the button here -->
+				<span class="screen-reader-text"><?php echo esc_html( 'NAME OF THE BUTTON HERE' ); ?></span>
+			</a><!-- .content-box-button -->
+		</div><!-- .content-box-content -->
+	
+	</div><!-- #post-## -->
 
-	</div><!-- .content-box-content -->
-</section><!-- #post-## -->
+</section>	
