5791c8ddb81432be94c45267108cd903cf299177
Audrobot <kornickiaa@gmail.com>
2018-03-24 12:45:59 -0700

    comment out search

--- themes/thestarfish/searchform.php
+++ themes/thestarfish/searchform.php
@@ -1,4 +1,4 @@
-<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+<!--<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 	<fieldset>
 		<label>
 			<input type="search" class="search-field" placeholder="SEARCH ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
@@ -10,4 +10,4 @@
 			<span class="screen-reader-text"><?php echo esc_html( 'Search' ); ?></span>
 		</button>
 	</fieldset>
-</form>
+</form>-->
