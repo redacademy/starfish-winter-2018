f8cbe121431980e24c6d24c1d1d375424a3d96b0
Audrobot <kornickiaa@gmail.com>
2018-03-25 14:12:41 -0700

    move get sidebar to div

--- themes/thestarfish/page.php
+++ themes/thestarfish/page.php
@@ -7,17 +7,19 @@
 
 get_header(); ?>
 
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+			
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
 			<?php endwhile; // End of the loop. ?>
-
+			
+			<div class="contact-sidebar"><?php get_sidebar(); ?></div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
