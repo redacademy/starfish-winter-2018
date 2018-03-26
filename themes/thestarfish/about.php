5013781492f5eb8a511ac41d8ac0f41788cfacf4
Veronika Kochetkova <veronica90k@gmail.com>
2018-03-26 00:00:48 -0700

    Style about us page

--- themes/thestarfish/about.php
+++ themes/thestarfish/about.php
@@ -24,42 +24,41 @@ get_header(); ?>
         </header><!-- .entry-header -->
 
 				<div class="entry-content">
+					<section class="about-carousels">
+						<?php
+							/**
+							* Set the Custom Field Suite Loops Working
+							*/
 							
-					<?php the_content(); ?>
-					<?php
-						/**
-						* Set the Custom Field Suite Loops Working
-						*/
-						
-						$carousels = CFS()->get( 'about_carousel' );
+							$carousels = CFS()->get( 'about_carousel' );
 
-						foreach ( $carousels as $carousel ) {
-							
-							$single_img = empty($carousel['about_carousel_cell'][0]['about_carousel_cell_image_secondary']); //check if the first cell in the carousel contains one or two imgs
+							foreach ( $carousels as $carousel ) {
+								
+								$single_img = empty($carousel['about_carousel_cell'][0]['about_carousel_cell_image_secondary']); //check if the first cell in the carousel contains one or two imgs
 
-							echo '<div class="carousel-container ' . ($single_img ? 'container-single' : 'container-double') . '">'; //if there is no secondary img -> add class container-single, otherwise -> add class container-double
-								//wrap in whole carousel
-								$carousel_cells = $carousel['about_carousel_cell'];
-								foreach($carousel_cells as $carousel_cell){
-									// loop for each carousel cell
-									
-									echo '<div class="carousel-cell-container' . ($single_img ? ' single' : ' double') . '">'; //if there is no secondary img -> add class single, otherwise -> double
-									//wrap in carousel cell with all the content and img
-										echo '<div class="carousel-cell-images' . ($single_img ? '' : ' carousel-double') . '">'; //if there is no secondary img -> add nothing, otherwise -> add class carousel-double
-											echo '<img src="' . $carousel_cell['about_carousel_cell_image_primary'] . '" class="carousel-cell-image-primary" alt="" />';
-											if ($single_img == false) { //if there is secondary img -> show the secondary img on screen
-												echo '<img src="' . $carousel_cell['about_carousel_cell_image_secondary'] . '" class="carousel-cell-image-secondary" alt="" />';
-											}
+								echo '<div class="carousel-container ' . ($single_img ? 'container-single' : 'container-double') . '">'; //if there is no secondary img -> add class container-single, otherwise -> add class container-double
+									//wrap in whole carousel
+									$carousel_cells = $carousel['about_carousel_cell'];
+									foreach($carousel_cells as $carousel_cell){
+										// loop for each carousel cell
+										
+										echo '<div class="carousel-cell-container' . ($single_img ? ' single' : ' double') . '">'; //if there is no secondary img -> add class single, otherwise -> double
+										//wrap in carousel cell with all the content and img
+											echo '<div class="carousel-cell-images' . ($single_img ? '' : ' carousel-double') . '">'; //if there is no secondary img -> add nothing, otherwise -> add class carousel-double
+												echo '<img src="' . $carousel_cell['about_carousel_cell_image_primary'] . '" class="carousel-cell-image-primary" alt="" />';
+												if ($single_img == false) { //if there is secondary img -> show the secondary img on screen
+													echo '<img src="' . $carousel_cell['about_carousel_cell_image_secondary'] . '" class="carousel-cell-image-secondary" alt="" />';
+												}
+											echo '</div>';
+											echo '<div class="carousel-cell-content">';
+												echo '<h2>' . $carousel['about_carousel_title'] . '</h2>';
+												echo $carousel_cell['about_carousel_cell_content'];
+											echo '</div>';
 										echo '</div>';
-										echo '<div class="carousel-cell-content">';
-											echo '<h2>' . $carousel['about_carousel_title'] . '</h2>';
-											echo $carousel_cell['about_carousel_cell_content'];
-										echo '</div>';
-									echo '</div>';
-								}
-							echo '</div>';
-						}
-					?>
+									}
+								echo '</div>';
+							}
+						?>
 
 					<section class="about-box-area">
 						
@@ -70,9 +69,9 @@ get_header(); ?>
 							</div><!-- .content-box-header -->
 
 							<div class="content-box-content">
-								<a class="content-box-button" href="<?php echo esc_url(get_permalink(get_page_by_path( 'team' ) ) ); ?>">Meet the team
+								<a class="btn-transparent" href="<?php echo esc_url(get_permalink(get_page_by_path( 'team' ) ) ); ?>">Meet the team
 									<span class="screen-reader-text"><?php echo esc_html( 'Meet The Team' ); ?></span>
-								</a><!-- .content-box-button -->
+								</a><!-- .btn-transparent -->
 							</div><!-- .content-box-content -->
 						
 						</div>
