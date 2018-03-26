b20f20db459753ea334a51be4eed588cc63268ac
Audrobot <kornickiaa@gmail.com>
2018-03-25 18:41:51 -0700

    add donate wrapper, content, x redirect

--- themes/thestarfish/donate.php
+++ themes/thestarfish/donate.php
@@ -12,12 +12,18 @@ wp_head(); ?>
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
-
-				<div class="donate-content">
-					<?php get_template_part( 'template-parts/content' ); ?>
-					<?php the_content(); ?>
+				
+				<div class="donate-wrapper">
+				<img src="<?php echo get_template_directory_uri() . '/images/donate-image.png'; ?>" class="donate-image" alt="Donate Image">
+					<div class="donate-content">
+						<a class="close-link" href="<?php echo esc_url(get_permalink(get_page_by_path( 'front-page' ) ) ); ?>" >	
+						X
+						</a>
+						<?php get_template_part( 'template-parts/content' ); ?>
+						<?php the_content(); ?>
+					</div>
 				</div>
-		
+			
 			<?php endwhile; ?>
 
 		</main><!-- #main -->
