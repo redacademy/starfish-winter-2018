e7706c70a068a5af70b0f59b81878dbdbad10177
Paul Dudsdeemaytha <pauldudsdeemaytha@Pauls-MacBook-Pro.local>
2018-03-25 22:06:54 -0700

    added template part for content-box

--- themes/thestarfish/template-parts/content-box.php
+++ themes/thestarfish/template-parts/content-box.php
@@ -16,7 +16,7 @@
 
 		<div class="content-box-content">
 			<!-- Content (description) for the box here -->
-			<a class="content-box-button" href="<?php echo esc_url(get_permalink(get_page_by_path( 'path to the page here' ) ) ); ?>"><!-- Name of the button here -->
+			<a class="btn-transparent" href="<?php echo esc_url(get_permalink(get_page_by_path( 'path to the page here' ) ) ); ?>"><!-- Name of the button here -->
 				<span class="screen-reader-text"><?php echo esc_html( 'NAME OF THE BUTTON HERE' ); ?></span>
 			</a><!-- .content-box-button -->
 		</div><!-- .content-box-content -->
